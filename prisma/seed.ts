25e2fb3597673f2e89466f17a80f09917037cd43
yabu1121 <n.hayato0122@outlook.jp>
2025-10-19 00:36:19 +0900

    database connect

---
+++ prisma/seed.ts
@@ -0,0 +1,54 @@
+//ダミーデータを作るためのファイル
+
+const { PrismaClient } = require("../src/generated/prisma");
+const bcrypt = require('bcryptjs');
+
+const prisma = new PrismaClient()
+
+const main = async () => {
+  // clean up 
+  await prisma.user.deleteMany()
+  await prisma.post.deleteMany()
+
+  const hashedPassword = await bcrypt.hash('password123', 12);
+  const dummyImage = [
+    'https://picsum.photos/seed/post1/600/400',
+    'https://picsum.photos/seed/post2/600/400',
+  ]
+
+  const user = await prisma.user.create({
+    data: {
+      email: 'test@example.com',
+      name: 'user1',
+      password: hashedPassword,
+      posts: {
+        create: [
+          {
+            title: 'first blog',
+            content: '最初の投稿です。',
+            topImage: dummyImage[1],
+            pubulished: true,
+          },
+          {
+            title: 'second blog',
+            content: '2個目の投稿です。どうもこんにちは',
+            topImage: dummyImage[1],
+            pubulished: true,
+          },
+        ]
+      }
+
+    }
+  })
+  console.log({ user })
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1); //TODO exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
+
---
+++ src/lib/prisma.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as { //TODO globalThis
+  prisma: PrismaClient | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient() //TODO ??
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
