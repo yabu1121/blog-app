efde0543e130fd7aae26189a99b5ce9ccd477780
yabu1121 <n.hayato0122@outlook.jp>
2025-10-23 20:10:08 +0900

    required_error change

--- src/validations/user.ts
+++ src/validations/user.ts
@@ -1,14 +1,14 @@
 import { object, string } from "zod";
 export const registerSchema = object({
   name: string().min(1,"名前は必須です"),
-  email: string({required_error: "メールアドレスは必須です"})
+  email: string()
   .min(1,"メールアドレスは必須です")
   .email("不正なメールアドレスです"),
-  password: string({required_error: "パスワードは必須です"})
+  password: string()
   .min(1,"パスワードは必須です")
   .min(8,"パスワードは最低8文字必要です")
   .max(32,"パスワードは最大32以内にしてください"),
-  confirmPassword:string({required_error: "確認用パスワードは必須です"})
+  confirmPassword:string()
   .min(1,"確認用パスワードは必須です")
 }).refine((data) => data.password === data.confirmPassword, {
   message: "パスワードが一致しません",
